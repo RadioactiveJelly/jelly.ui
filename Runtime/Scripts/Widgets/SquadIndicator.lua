6ca564716cc08b9f9d579a82f325926a2a39dc22
RadioactiveJelly <radioactivesticker@gmail.com>
2025-09-03 16:29:24 +0800

    Added current orders to SquadIndicator

--- Runtime/Scripts/Widgets/SquadIndicator.lua
+++ Runtime/Scripts/Widgets/SquadIndicator.lua
@@ -2,7 +2,22 @@
 behaviour("SquadIndicator")
 
 function SquadIndicator:Start()
+	self.squadCount = 0
+	self.squadOrder = ""
+
+	GameEvents.onActorSpawn.AddListener(self,"OnActorSpawn")
+	self.hasSpawned = false
+end
+
+function SquadIndicator:OnActorSpawn(actor)
+	if not actor.isPlayer then return end
+	if self.hasSpawned then return end
+
 	self.script.AddValueMonitor("MonitorPlayerSquad", "OnSquadCountChanged")
+	self.script.AddValueMonitor("MonitorPlayerOrderState", "OnSquadOrderChanged")
+	self.squadOrder = string.upper(tostring(PlayerHud.playerOrderState))
+	
+	self.hasSpawned = true
 end
 
 function SquadIndicator:MonitorPlayerSquad()
@@ -12,10 +27,37 @@ function SquadIndicator:MonitorPlayerSquad()
 	return #Player.squad.members - 1
 end
 
+function SquadIndicator:MonitorPlayerOrderState()
+	if PlayerHud == nil then return end
+
+	return PlayerHud.playerOrderState
+end
+
 function SquadIndicator:OnSquadCountChanged(count)
 	if count == nil then return end
 
-	if count == 0 then self.targets.Indicator.text = "NO SQUAD" return end
+	self.squadCount = count
+	self:UpdateText()
+end
 
-	self.targets.Indicator.text = "SQUAD: " .. count
+
+function SquadIndicator:OnSquadOrderChanged(order)
+	if order == nil then return end
+
+	self.squadOrder = string.upper(tostring(order))
+	self:UpdateText()
+end
+
+function SquadIndicator:UpdateText()
+	local text = "NO SQUAD"
+	
+	if self.squadCount > 0 then
+		if self.squadOrder ~= "" then
+			text = "SQUAD(" .. self.squadCount .. "): " .. self.squadOrder
+		else
+			text = "SQUAD(" .. self.squadCount .. ")"
+		end
+	end
+
+	self.targets.Indicator.text = text
 end
