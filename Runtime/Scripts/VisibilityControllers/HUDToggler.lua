4d0394fefce292aa7f73b7a5ec588987df85c0b4
RadioactiveJelly <radioactivesticker@gmail.com>
2025-09-04 19:04:11 +0800

    Added HUDToggler

---
+++ Runtime/Scripts/VisibilityControllers/HUDToggler.lua
@@ -0,0 +1,16 @@
+-- Register the behaviour
+behaviour("HUDToggler")
+
+function HUDToggler:Start()
+	self.script.AddValueMonitor("MonitorHudVisibility", "OnVisibilityChanged")
+end
+
+function HUDToggler:MonitorHudVisibility()
+	return GameManager.hudPlayerEnabled
+end
+
+function HUDToggler:OnVisibilityChanged(isVisible)
+	local alpha = 0
+	if isVisible then alpha = 1 end
+	self.targets.CanvasGroup.alpha = alpha
+end
